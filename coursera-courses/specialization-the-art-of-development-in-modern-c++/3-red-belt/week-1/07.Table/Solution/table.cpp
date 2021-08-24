a8932c86f902984b17ab3f07d56bfce74c13d46a
Xelerezex <nikitavalderman@gmail.com>
2021-08-24 18:52:11 +0300

    C++. Red Belt. Week 1. 07.Table. Writing Class Structure until 4. task.

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-1/07.Table/Solution/table.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-1/07.Table/Solution/table.cpp
@@ -3,20 +3,31 @@
 using namespace std;
 
 // Реализуйте здесь шаблонный класс Table
+template<typename T>
+class Table {
+    public:
+        Table(const size_t rows, const size_t columns) : Rows(rows), Columns(columns) {}
+
+    private:
+        T element;
+        size_t Rows;
+        size_t Columns;
+};
+
 
 void TestTable() {
-  Table<int> t(1, 1);
-  ASSERT_EQUAL(t.Size().first, 1u);
-  ASSERT_EQUAL(t.Size().second, 1u);
-  t[0][0] = 42;
-  ASSERT_EQUAL(t[0][0], 42);
-  t.Resize(3, 4);
-  ASSERT_EQUAL(t.Size().first, 3u);
-  ASSERT_EQUAL(t.Size().second, 4u);
+    Table<int> t(1, 1);
+    ASSERT_EQUAL(t.Size().first, 1u);
+    ASSERT_EQUAL(t.Size().second, 1u);
+    t[0][0] = 42;
+    ASSERT_EQUAL(t[0][0], 42);
+    t.Resize(3, 4);
+    ASSERT_EQUAL(t.Size().first, 3u);
+    ASSERT_EQUAL(t.Size().second, 4u);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestTable);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestTable);
+    return 0;
 }
