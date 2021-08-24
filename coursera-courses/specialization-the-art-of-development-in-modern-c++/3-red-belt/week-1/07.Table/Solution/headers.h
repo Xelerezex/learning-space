04aa6f9f10eaa049a17505c8b1bd015489d9b670
Xelerezex <nikitavalderman@gmail.com>
2021-08-24 18:47:36 +0300

    C++. Red Belt. Week 1. 07.Table. Adding folders & project structure.

---
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-1/07.Table/Solution/headers.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//-----------------DATA-TYPES
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <deque>
+#include <utility>
+
+//-----------------ALGORITHMIC
+#include <algorithm>
+
+//----------------INPUT-OUTPUT
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+//----------------EXCEPTIONS
+#include <exception>
+#include <stdexcept>
+
+//----------------MEMORY/TIME
+#include <memory>
+#include <chrono>
+
+//----------------NAMESPACES
+using namespace std::chrono;
+using namespace std;
+
