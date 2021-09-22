3b8a1f97c50ef77bcf0d1503fd509c7f65284147
Xelerezex <nikitavalderman@gmail.com>
2021-09-22 14:47:21 +0300

    Updating .gitignore

---
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-1/02-maximum-pairwise-product/Solution/maximum-pairwise-product.cpp
@@ -0,0 +1,14 @@
+// #include <iostream>
+// #include <vector>
+#include "test.h"
+
+
+using namespace std;
+
+int main() {
+    int smth;
+    while(cin >> smth) {
+        cout << smth << endl;
+    }
+
+}
---
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-1/02-maximum-pairwise-product/Solution/test.h
@@ -0,0 +1,5 @@
+#include "test_runner.h"
+#include "profile.h"
+
+void TestBasic();
+void TestAll();
