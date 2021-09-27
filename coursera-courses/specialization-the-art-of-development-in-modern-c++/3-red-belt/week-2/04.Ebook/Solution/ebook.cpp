a7198aae558c6b8bf3c0c1c7bbd7f0a70ab7ea91
Xelerezex <nikitavalderman@gmail.com>
2021-09-27 19:53:51 +0300

    C++. Red Belt. Week 2. 04.Ebook. Fully Done.

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-2/04.Ebook/Solution/ebook.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-2/04.Ebook/Solution/ebook.cpp
@@ -1,76 +1,46 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <set>
 #include <utility>
 
 using namespace std;
 
-class ReadingManager {
+class ReadingManager
+{
 public:
-    ReadingManager()
-            : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-                sorted_users_(),
-                user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+    ReadingManager() :
+    people(MAX_USER_COUNT_ + 1, -1),
+    pages(MAX_PAGE_COUNT_ + 1, 0) {}
 
-    void Read(int user_id, int page_count) {
-        if (user_page_counts_[user_id] == 0) {
-            AddUser(user_id);
+    void Read(int user_id, int page_count)
+    {
+        for (int i = people[user_id] + 1; i <= page_count; i++)
+        {
+            ++pages[i];
         }
-        user_page_counts_[user_id] = page_count;
-        int& position = user_positions_[user_id];
-        while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-            SwapUsers(position, position - 1);
-        }
+        people[user_id] = page_count;
     }
 
-    double Cheer(int user_id) const {
-        if (user_page_counts_[user_id] == 0) {
+    double Cheer(int user_id) const
+    {
+        if (people[user_id] == -1)
+        {
             return 0;
         }
-        const int user_count = GetUserCount();
-        if (user_count == 1) {
+
+        if (pages[0] == 1)
+        {
             return 1;
         }
-        const int page_count = user_page_counts_[user_id];
-        int position = user_positions_[user_id];
-        while (position < user_count &&
-            user_page_counts_[sorted_users_[position]] == page_count) {
-            ++position;
-        }
-        if (position == user_count) {
-                return 0;
-        }
-        // По умолчанию деление целочисленное, поэтому
-        // нужно привести числитель к типу double.
-        // Простой способ сделать это — умножить его на 1.0.
-        return (user_count - position) * 1.0 / (user_count - 1);
+        return static_cast<double>(pages[0] - pages[people[user_id]]) / static_cast<double>(pages[0] - 1);
     }
-
 private:
-    // Статическое поле не принадлежит какому-то конкретному
-    // объекту класса. По сути это глобальная переменная,
-    // в данном случае константная.
-    // Будь она публичной, к ней можно было бы обратиться снаружи
-    // следующим образом: ReadingManager::MAX_USER_COUNT.
     static const int MAX_USER_COUNT_ = 100'000;
+    static const int MAX_PAGE_COUNT_ = 1'000;
 
-    vector<int> user_page_counts_;
-    vector<int> sorted_users_;     // отсортированы по убыванию количества страниц
-    vector<int> user_positions_; // позиции в векторе sorted_users_
-
-    int GetUserCount() const {
-        return sorted_users_.size();
-    }
-    void AddUser(int user_id) {
-        sorted_users_.push_back(user_id);
-        user_positions_[user_id] = sorted_users_.size() - 1;
-    }
-    void SwapUsers(int lhs_position, int rhs_position) {
-        const int lhs_id = sorted_users_[lhs_position];
-        const int rhs_id = sorted_users_[rhs_position];
-        swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-        swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-    }
+    std::vector<int> people;
+    std::vector<int> pages;
 };
 
 
