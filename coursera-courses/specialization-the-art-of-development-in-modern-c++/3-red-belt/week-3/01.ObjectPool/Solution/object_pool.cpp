b256ba98a058f41695e1ac7b75f21b6521aacb70
Xelerezex <nikitavalderman@gmail.com>
2021-10-04 17:41:01 +0300

    C++. Red Belt. Week 3. 01.ObjectPool FULLY DONE!

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/01.ObjectPool/Solution/object_pool.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/01.ObjectPool/Solution/object_pool.cpp
@@ -1,13 +1,13 @@
 #include "test_runner.h"
-/*
+
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <queue>
+#include <deque>
 #include <stdexcept>
 #include <set>
 using namespace std;
-*/
+
 
 template <class T>
 class ObjectPool {
@@ -15,71 +15,60 @@ class ObjectPool {
         T* Allocate() {
             if (released.empty()) {
                 T* pNewObj = new T;
-                allocated.push(pNewObj);
+                allocated.insert(pNewObj);
                 return pNewObj;
             } else {
-                T* pOldObj = released.front();
-                allocated.push(pOldObj);
-                released.pop();
-
-                return pOldObj;
+                auto [obj, flag] = allocated.insert(released.front());
+                released.pop_front();
+                return *obj;
             }
         }
 
 
         T* TryAllocate() {
             if (released.empty()) {
-                T* pNewObj = new T;
-                allocated.push(pNewObj);
-                return pNewObj;
-            } else {
                 return nullptr;
+            } else {
+                auto [obj, flag] = allocated.insert(released.front());
+                released.pop_front();
+                return *obj;
             }
         }
 
 
         void Deallocate(T* object) {
-            queue<T*> temporary;
-            temporary.swap(released);
+            auto obj = allocated.find(object);
 
-            while(!temporary.empty()) {
-                if (temporary.front() == object) {
-                    released.push(temporary.front());
-
-                    delete temporary.front();
-                    temporary.pop();
-                }
+            if (obj == end(allocated)) {
+                throw invalid_argument("");
+            } else {
+                released.push_back(*obj);
+                allocated.erase(obj);
             }
 
-
-/*            if (temporary.size() >= 1) {
-                delete temporary.front();
-                temporary.pop();
-
-                throw invalid_argument("");
-            }*/
-
-            released.swap(temporary);
         }
 
         size_t GetSizeAllocated() { return allocated.size(); }
         size_t GetSizeReleased()  { return released.size(); }
 
         ~ObjectPool() {
-            while(!allocated.empty()) {
-                delete allocated.front();
-                allocated.pop();
+            while (!allocated.empty()) {
+                auto it = allocated.begin();
+                delete *it;
+                allocated.erase(it);
             }
 
-            while(!released.empty()) {
-                delete released.front();
-                released.pop();
+            while (!released.empty()) {
+                auto it = released.front();
+                delete it;
+                released.pop_front();
             }
+
         }
 
     private:
-        queue<T*> allocated;
-        queue<T*> released;
+        set<T*>   allocated;
+        deque<T*> released;
 };
 
 
@@ -87,29 +76,52 @@ class ObjectPool {
 void TestObjectPool() {
     ObjectPool<string> pool;
 
+    // --------------------------------------------------------------------------------------------//
     auto p1 = pool.Allocate();
     auto p2 = pool.Allocate();
-    auto p3 = pool.Allocate();
-
+    auto p3 = pool.Allocate();                  // alloc: 3, real: 0
     ASSERT_EQUAL(pool.GetSizeAllocated(), 3);
-
+    ASSERT_EQUAL(pool.GetSizeReleased(), 0);
+    // --------------------------------------------------------------------------------------------//
     *p1 = "first";
     *p2 = "second";
-    *p3 = "third";
-
-    pool.Deallocate(p2);
+    *p3 = "third";                              // alloc: 3, real: 0
+    // --------------------------------------------------------------------------------------------//
+    pool.Deallocate(p2);                        // alloc: 2, real: 1  {first, third} {second}
     ASSERT_EQUAL(pool.GetSizeAllocated(), 2);
     ASSERT_EQUAL(pool.GetSizeReleased(), 1);
-
-
-    ASSERT_EQUAL(*pool.Allocate(), "second");
-
-    pool.Deallocate(p3);
-    pool.Deallocate(p1);
-    ASSERT_EQUAL(*pool.Allocate(), "third");
-    ASSERT_EQUAL(*pool.Allocate(), "first");
-
-    pool.Deallocate(p1);
+    // --------------------------------------------------------------------------------------------//
+    ASSERT_EQUAL(*pool.Allocate(), "second");   // alloc: 3, real: 0  {first, third, second} {}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 3);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 0);
+    // --------------------------------------------------------------------------------------------//
+    pool.Deallocate(p3);                        // alloc: 2, real: 1  {first, second} {third}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 2);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 1);
+    // --------------------------------------------------------------------------------------------//
+    pool.Deallocate(p1);                        // alloc: 1, real: 2  {second} {third, first}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 1);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 2);
+    // --------------------------------------------------------------------------------------------//
+    ASSERT_EQUAL(*pool.Allocate(), "third");   // alloc: 2, real: 1  {second, *third} {first}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 2);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 1);
+    // --------------------------------------------------------------------------------------------//
+    ASSERT_EQUAL(*pool.Allocate(), "first");   // alloc: 3, real: 0  {second, third, *first} {}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 3);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 0);
+    // --------------------------------------------------------------------------------------------//
+    pool.Deallocate(p1);                       // alloc: 2, real: 1  {second, third} {first}
+    ASSERT_EQUAL(pool.GetSizeAllocated(), 2);
+    ASSERT_EQUAL(pool.GetSizeReleased(), 1);
+    // --------------------------------------------------------------------------------------------//
+    string* p4 = new string("fourth");
+    try {
+        pool.Deallocate(p4);
+        ASSERT_EQUAL(0, 1);
+    } catch (exception& ex){
+        ASSERT_EQUAL(1, 1);
+    }
 }
 
 int main() {
