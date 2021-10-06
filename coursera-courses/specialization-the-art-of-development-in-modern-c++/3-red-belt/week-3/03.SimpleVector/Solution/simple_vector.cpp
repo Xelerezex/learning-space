805292bfe440c5a2e4684e67173a93e3e94393e5
Xelerezex <nikitavalderman@gmail.com>
2021-10-06 19:02:42 +0300

    C++. Red Belt. Week 3. 03.SimpleVector FULLY DONE!

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/03.SimpleVector/Solution/simple_vector.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/03.SimpleVector/Solution/simple_vector.cpp
@@ -7,38 +7,98 @@
 #include <string>
 using namespace std;
 
+//-------------FOR-VECTOR
+template <class L>
+ostream& operator << (ostream& os, SimpleVector <L>& l) {
+    for (auto &item : l) {
+        if (l.Size() == 1) {
+            os << "[" << item << "]";
+        } else if (item == *l.begin()){
+            os << "[" << item << ", ";
+        } else if (item == *(l.end() - 1)) {
+            os << item << "]";
+        } else {
+            os << item << ", ";
+        }
+    }
+    return os;
+}
+
 void TestConstruction() {
     SimpleVector<int> empty;
     ASSERT_EQUAL(empty.Size(), 0u);
     ASSERT_EQUAL(empty.Capacity(), 0u);
     ASSERT(empty.begin() == empty.end());
 
+
     SimpleVector<string> five_strings(5);
     ASSERT_EQUAL(five_strings.Size(), 5u);
     ASSERT(five_strings.Size() <= five_strings.Capacity());
+
     for (auto& item : five_strings) {
         ASSERT(item.empty());
     }
+
     five_strings[2] = "Hello";
+    five_strings[4] = "Not Hello";
+
     ASSERT_EQUAL(five_strings[2], "Hello");
+    ASSERT_EQUAL(five_strings[4], "Not Hello");
 }
 
 void TestPushBack() {
-    SimpleVector<int> v;
-    for (int i = 10; i >= 1; --i) {
-        v.PushBack(i);
-        ASSERT(v.Size() <= v.Capacity());
-    }
-    sort(begin(v), end(v));
+    {
+        SimpleVector<int> v;
+        v.PushBack(7);
+        ASSERT_EQUAL(v.Size(), 1);
+        ASSERT_EQUAL(v.Size(), v.Capacity());
+        ASSERT_EQUAL(v[0], 7);
+        cout << v << " end = " << *(v.end() - 1) << endl;
 
-    const vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    ASSERT_EQUAL(v.Size(), expected.size());
-    ASSERT(equal(begin(v), end(v), begin(expected)));
+        v.PushBack(77);
+        ASSERT_EQUAL(v.Size(), 2);
+        ASSERT_EQUAL(v.Capacity(), 2);
+        ASSERT_EQUAL(v[1], 77);
+        cout << v << " end = " << *(v.end() - 1) << endl;
+
+        v.PushBack(777);
+        ASSERT_EQUAL(v.Size(), 3);
+        ASSERT_EQUAL(v.Capacity(), 4);
+        ASSERT_EQUAL(v[2], 777);
+        cout << v << " end = " << *(v.end() - 1) << endl;
+
+        v.PushBack(7777);
+        ASSERT_EQUAL(v.Size(), 4);
+        ASSERT_EQUAL(v.Capacity(), 4);
+        ASSERT_EQUAL(v[3], 7777);
+        cout << v << " end = " << *(v.end() - 1) << endl;
+
+        v.PushBack(77777);
+        ASSERT_EQUAL(v.Size(), 5);
+        ASSERT_EQUAL(v.Capacity(), 8);
+        ASSERT_EQUAL(v[4], 77777);
+        cout << v << " end = " << *(v.end() - 1) << endl;
+    }
+        cout << "-------------------------\n";
+    {
+        SimpleVector<int> v;
+        for (int i = 10; i >= 1; --i) {
+            v.PushBack(i);
+            ASSERT(v.Size() <= v.Capacity());
+        }
+        sort(begin(v), end(v));
+
+        const vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        ASSERT_EQUAL(v.Size(), expected.size());
+        ASSERT(equal(begin(v), end(v), begin(expected)));
+    }
 }
+
 
 int main() {
     TestRunner tr;
     RUN_TEST(tr, TestConstruction);
     RUN_TEST(tr, TestPushBack);
+
     return 0;
 }
--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/03.SimpleVector/Solution/simple_vector.h
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-3/03.SimpleVector/Solution/simple_vector.h
@@ -1,24 +1,91 @@
 #pragma once
 
 #include <cstdlib>
+#include <iostream>
 
 // Реализуйте шаблон SimpleVector
 template <typename T>
 class SimpleVector {
 public:
-    SimpleVector();
-    explicit SimpleVector(size_t size);
-    ~SimpleVector();
+    SimpleVector() : Data(new T[0]), SizeVal(0), CapacityVal(SizeVal * 2) {
+    }
 
-    T& operator[](size_t index);
+    explicit SimpleVector(size_t size) : Data(new T[size]), SizeVal(size), CapacityVal(size){
+    }
 
-    T* begin();
-    T* end();
 
-    size_t Size() const;
-    size_t Capacity() const;
-    void PushBack(const T& value);
+    T& operator[](size_t index) {
+        return *(Data + index);
+    }
+
+
+    T* begin() {
+        return Data;
+    }
+
+    T* end() {
+        return Data + SizeVal;
+    }
+
+    size_t Size() const {
+        return SizeVal;
+    }
+
+    size_t Capacity() const {
+        return CapacityVal;
+    }
+
+    void PushBack(const T& value) {
+        bool cond1 = (CapacityVal == 0 && SizeVal == 0);
+        bool cond2 = (SizeVal == CapacityVal);
+        bool cond3 = (SizeVal < CapacityVal);
+
+
+        if (cond1) {
+            Data[0] = value;
+            ++CapacityVal;
+            ++SizeVal;
+
+            // std::cerr << "First" << std::endl;
+
+        } else if (!cond1 && cond2) {
+            // New Capacity
+            T* temporary = new T[SizeVal + 1];
+            CapacityVal *=  2;
+
+            for (int it = 0; it < SizeVal; ++it) {
+                temporary[it] = Data[it];
+            }
+
+            temporary[SizeVal] = value;
+            ++SizeVal;
+
+            delete[] Data;
+
+            Data = new T[CapacityVal];
+            for (int it = 0; it < SizeVal; ++it) {
+                Data[it] = temporary[it];
+            }
+
+            // Cleaning temporary massive
+            delete[] temporary;
+
+            // std::cerr << "Second" << std::endl;
+
+        } else if (!cond1 && cond3) {
+            Data[SizeVal] = value;
+            ++SizeVal;
+
+            // std::cerr << "Third" << std::endl;
+        }
+    }
+
+    ~SimpleVector() {
+        delete[] Data;
+    }
 
 private:
-    // Добавьте поля для хранения данных вектора
+    T* Data;
+    size_t SizeVal;
+    size_t CapacityVal;
 };
