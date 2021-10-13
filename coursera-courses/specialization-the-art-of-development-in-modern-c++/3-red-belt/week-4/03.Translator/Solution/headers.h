109c101f9a21850e740d4cf6f768dd64fa6db563
Xelerezex <nikitavalderman@gmail.com>
2021-10-13 18:24:32 +0300

    C++. Red Belt. Week 4. 03.Translator Added project structure

---
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-4/03.Translator/Solution/headers.h
@@ -0,0 +1,40 @@
+#pragma once
+
+//-----------------DATA-TYPES
+#include <utility>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <list>
+#include <set>
+#include <map>
+
+//-----------------MATH
+#include <cmath>
+
+//-----------------ALGORITHMIC
+#include <algorithm>
+#include <numeric>
+
+//----------------INPUT-OUTPUT
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+//----------------EXCEPTIONS
+#include <exception>
+#include <stdexcept>
+
+//----------------MEMORY/TIME
+#include <memory>
+#include <chrono>
+
+//----------------ITERATION
+#include <iterator>
+
+//----------------NAMESPACES
+using namespace std::chrono;
+using namespace std;
+
