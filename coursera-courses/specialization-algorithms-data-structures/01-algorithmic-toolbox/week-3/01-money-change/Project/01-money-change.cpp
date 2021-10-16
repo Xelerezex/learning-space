fbcca2d95a1836c753d165b4948eb928a60fd4ab
Xelerezex <nikitavalderman@gmail.com>
2021-10-16 17:27:56 +0300

    Algorithms. Course 1. Week 3. 01-money-change FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/01-money-change/Project/01-money-change.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/01-money-change/Project/01-money-change.cpp
@@ -1,6 +1,40 @@
 #include "test_runner.h"
 #include "profile.h"
 
-int main() {
+unsigned change(unsigned coins) {
+    unsigned count = 0;
 
+    while (coins > 0) {
+        if (coins >= 10) {
+            coins -= 10;
+            ++count;
+        } else if (coins >= 5) {
+            coins -= 5;
+            ++count;
+        } else {
+            coins -= 1;
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    {
+        LOG_DURATION("Change from 2");
+        ASSERT_EQUAL(change(2), 2);
+    }
+    {
+        LOG_DURATION("Change from 28");
+        ASSERT_EQUAL(change(28), 6);
+    }
+    {
+        LOG_DURATION("Change from 999");
+        ASSERT_EQUAL(change(999), 104);
+    }
+    {
+        LOG_DURATION("Change from 1000");
+        ASSERT_EQUAL(change(1000), 100);
+    }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/01-money-change/Solution/name.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/01-money-change/Solution/name.cpp
@@ -1,3 +1,26 @@
-int main() {
+#include <iostream>
 
+unsigned change(unsigned coins) {
+    unsigned count = 0;
+
+    while (coins > 0) {
+        if (coins >= 10) {
+            coins -= 10;
+            ++count;
+        } else if (coins >= 5) {
+            coins -= 5;
+            ++count;
+        } else {
+            coins -= 1;
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    unsigned coins = 0;
+    std::cin >> coins;
+    std::cout << change(coins) << std::endl;
 }
