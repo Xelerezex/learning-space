5c8dbf84f61bb9757662b24d148b58600a5fa4ca
Xelerezex <nikitavalderman@gmail.com>
2021-11-08 17:04:48 +0300

    Algorithms. Course 1. Week 3. 02-max-val-of-loot FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/02-max-val-of-loot/Project/02-max-val-of-loot.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/02-max-val-of-loot/Project/02-max-val-of-loot.cpp
@@ -1,6 +1,50 @@
 #include "test_runner.h"
 #include "profile.h"
 
-int main() {
+double max_value(vector<pair<double, double>> &items, double MAX_W) {
+    sort(items.begin(), items.end(),
+        [](pair<double, double> pr1, pair<double, double> pr2){
+            return (pr1.first / pr1.second) > (pr2.first / pr2.second);
+        }
+    );
 
+    double output = 0.0000;
+
+    for (size_t it = 0; it < items.size(); ++it) {
+        if (items[it].second <= MAX_W) {
+            MAX_W -= items[it].second;
+            output += items[it].first;
+        } else {
+            output += (items[it].first / items[it].second) * MAX_W;
+            break;
+        }
+    }
+
+    ostringstream convert;
+    convert.precision(7); // 10
+    convert << output;
+    return stod(convert.str());
+}
+
+int main() {
+    {
+        LOG_DURATION("First test");
+        double /*QUANTITY = 3,*/ MAX_W = 50;
+        // Get sorted by first / second
+        vector<pair<double, double>> input = {
+            {120, 30},  // 4
+            {60, 20},   // 3
+            {100, 50},  // 2
+        };
+        ASSERT_EQUAL(max_value(input, MAX_W), 180.0000);
+    }
+    {
+        LOG_DURATION("Second test");
+        double /*QUANTITY = 1,*/ MAX_W = 10;
+        // Get sorted by first / second
+        vector<pair<double, double>> input = {
+            {500, 30},
+        };
+        ASSERT_EQUAL(max_value(input, MAX_W), 166.6667);
+    }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/02-max-val-of-loot/Solution/02-max-val-of-loot.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/02-max-val-of-loot/Solution/02-max-val-of-loot.cpp
@@ -1,3 +1,56 @@
-int main() {
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
+using namespace std;
+
+double max_value(vector<pair<double, double>> &items, double MAX_W) {
+    sort(items.begin(), items.end(),
+        [](pair<double, double> pr1, pair<double, double> pr2){
+            return (pr1.first / pr1.second) > (pr2.first / pr2.second);
+        }
+    );
+
+    double output = 0.0;
+
+    for (size_t it = 0; it < items.size(); ++it) {
+        if (items[it].second <= MAX_W) {
+            MAX_W -= items[it].second;
+            output += items[it].first;
+        } else {
+            output += (items[it].first / items[it].second) * MAX_W;
+            break;
+        }
+    }
+
+/*    ostringstream convert;
+    convert.precision(7);
+    convert << output;
+    return stod(convert.str());*/
+    return output;
+}
+
+int main() {
+    double vec_size = 0, MAX_W = 0, first, second;
+    cin >> vec_size >> MAX_W;
+
+    vector<pair<double, double>> input(vec_size);
+
+
+    for (size_t it = 0; it < vec_size; ++it) {
+        cin >> first >> second;
+        input[it] = make_pair(first, second);
+    }
+
+    sort(input.begin(), input.end(),
+        [](pair<int, int> pr1, pair<int, int> pr2){
+            return (pr1.first / pr1.second) > (pr2.first / pr2.second);
+        }
+    );
+
+    double OUT = max_value(input, MAX_W);
+    cout.precision(10);
+    cout << OUT << endl;
 }
