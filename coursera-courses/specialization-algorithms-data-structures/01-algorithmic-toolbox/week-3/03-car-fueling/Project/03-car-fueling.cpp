76cb83f2355e3461726efeeabd478fd008820ed5
Xelerezex <nikitavalderman@gmail.com>
2021-11-10 16:19:09 +0300

    Algorithms. Course 1. Week 3. 03-car-fueling FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/03-car-fueling/Project/03-car-fueling.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/03-car-fueling/Project/03-car-fueling.cpp
@@ -1,23 +1,24 @@
 #include "test_runner.h"
 #include "profile.h"
 
-int compute_min_refills(uint dist, uint tank, vector<uint> &stops) {
+int compute_min_refills(int dist, int tank, vector<int> &stops) {
     int qua = 0;
-    uint distance = 0, tank_deriv = tank;
-    for (auto &stop: stops) {
-        distance = stop - distance;
-        if (distance > tank) {
+    int index = 0, last_stop = 0;
+    stops.push_back(dist);
+
+    while (index < stops.size()) {
+        cerr << "qua: " << qua << " index: " << index << " last_stop: " << last_stop << endl;
+        if (stops[index] - last_stop <= tank) {
+            cerr << "\nOK" << endl;
+            ++index;
+            continue;
+        } else if ((stops[index] - stops[index - 1] > tank) || (index == 0)) {
             return -1;
-        }
-
-        tank_deriv = tank_deriv - distance;
-
-        if (tank_deriv <= distance) {
-            tank_deriv = tank;
-            ++qua;
+        } else {
+            last_stop = stops[index - 1];
+            qua += 1;
         }
     }
-
     return qua;
 }
 
@@ -25,10 +26,14 @@ int compute_min_refills(uint dist, uint tank, vector<uint> &stops) {
 int main() {
     {
         LOG_DURATION("Stops: 200 375 550 750");
-        ASSERT_EQUAL(compute_min_refills(950, 400, {200, 375, 550, 750}), 2);
+        int dist = 950, tank = 400;
+        vector<int> stops = {200, 375, 550, 750};
+        ASSERT_EQUAL(compute_min_refills(dist, tank, stops), 2);
     }                                            // 200  25
     {                                            // 200  175
         LOG_DURATION("Stops: 1 2 5 9");
-        ASSERT_EQUAL(compute_min_refills(10, 3, {1, 2, 5, 9}), -1);
+        int dist = 10, tank = 3;
+        vector<int> stops = {1, 2, 5, 9};
+        ASSERT_EQUAL(compute_min_refills(dist, tank, stops), -1);
     }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/03-car-fueling/Solution/03-car-fueling.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/03-car-fueling/Solution/03-car-fueling.cpp
@@ -1,3 +1,46 @@
-int main() {
+#include <iostream>
+#include <vector>
 
+using std::endl;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::max;
+
+int compute_min_refills(int dist, int tank, vector<int> &stops) {
+    int qua = 0;
+    int index = 0, last_stop = 0;
+    stops.push_back(dist);
+
+    while (index < stops.size()) {
+        if (stops[index] - last_stop <= tank) {
+            ++index;
+            continue;
+        } else if ((stops[index] - stops[index - 1] > tank) || (index == 0)) {
+            return -1;
+        } else {
+            last_stop = stops[index - 1];
+            qua += 1;
+        }
+    }
+    return qua;
+}
+
+
+
+int main() {
+    int d = 0;
+    cin >> d;
+    int m = 0;
+    cin >> m;
+    int n = 0;
+    cin >> n;
+
+    vector<int> stops(n);
+    for (size_t i = 0; i < n; ++i)
+        cin >> stops.at(i);
+
+    cout << compute_min_refills(d, m, stops) << "\n";
+
+    return 0;
 }
