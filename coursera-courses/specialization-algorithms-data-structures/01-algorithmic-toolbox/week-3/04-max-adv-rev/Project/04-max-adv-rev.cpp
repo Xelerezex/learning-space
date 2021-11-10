5c7dc5dd1510758c2061dd31f59bd3330363376f
Xelerezex <nikitavalderman@gmail.com>
2021-11-10 16:32:22 +0300

    Algorithms. Course 1. Week 3. 04-max-adv-rev. Created tests.

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/04-max-adv-rev/Project/04-max-adv-rev.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/04-max-adv-rev/Project/04-max-adv-rev.cpp
@@ -1,6 +1,37 @@
 #include "test_runner.h"
 #include "profile.h"
 
-int main() {
+long long max_dot_product(vector<int> a, vector<int> b) {
+  long long result = 0;
 
+  return result;
+}
+
+int main() {
+    {
+        LOG_DURATION("First Test (One pair):");
+        vector<int>
+        first = {
+            23
+        },
+        second = {
+            39
+        };
+        ASSERT_EQUAL(max_dot_product(first, second), 897);
+    }
+    {
+        LOG_DURATION("Second Test (Three pairs)");
+        vector<int>
+        first = {
+            1, 3, -5
+        },
+        second = {
+            -2, 4, 1,
+        };
+        ASSERT_EQUAL(max_dot_product(first, second), 23);
+    }
+    {
+        LOG_DURATION("Max value Test: (Create after prog. works)");
+        ASSERT_EQUAL(1, -1);
+    }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/04-max-adv-rev/Solution/04-max-adv-rev.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/04-max-adv-rev/Solution/04-max-adv-rev.cpp
@@ -1,3 +1,23 @@
-int main() {
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
+using std::vector;
+
+long long max_dot_product(vector<int> a, vector<int> b) {
+    // write your code here
+    return -1;
+}
+
+int main() {
+    size_t n;
+    std::cin >> n;
+    vector<int> a(n), b(n);
+    for (size_t i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    for (size_t i = 0; i < n; i++) {
+        std::cin >> b[i];
+    }
+    std::cout << max_dot_product(a, b) << std::endl;
 }
