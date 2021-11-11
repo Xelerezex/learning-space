d15bb17dff49e4b7c1135f807b1051d1f46e481b
Xelerezex <nikitavalderman@gmail.com>
2021-11-11 17:27:17 +0300

    Algorithms. Course 1. Week 3. 05-collect-sign. Added tests.

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/05-collect-sign/Project/05-collect-sign.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-3/05-collect-sign/Project/05-collect-sign.cpp
@@ -1,6 +1,44 @@
 #include "test_runner.h"
 #include "profile.h"
 
-int main() {
 
+struct Segment {
+    int start, end;
+};
+
+vector<int> optimal_points(vector<Segment> &segments) {
+    vector<int> points;
+    //write your code here
+    for (size_t i = 0; i < segments.size(); ++i) {
+        points.push_back(segments[i].start);
+        points.push_back(segments[i].end);
+    }
+    return points;
+}
+
+int main() {
+    {
+        vector<Segment> test = {
+            {1, 3},
+            {2, 5},
+            {3, 6},
+        };
+        vector<int> expected = {
+            3
+        };
+        LOG_DURATION("First Test:");
+        ASSERT_EQUAL(optimal_points(test), expected);
+    }
+    {
+        vector<Segment> test = {
+            {4, 7},
+            {1, 3},
+            {5, 6},
+        };
+        vector<int> expected = {
+            3, 6
+        };
+        LOG_DURATION("Second Test:");
+        ASSERT_EQUAL(optimal_points(test), expected);
+    }
 }
