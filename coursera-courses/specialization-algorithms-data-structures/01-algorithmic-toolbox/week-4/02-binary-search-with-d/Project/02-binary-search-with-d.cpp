267b037c7e574999062e5c2bffab076f4a47fe52
Xelerezex <nikitavalderman@gmail.com>
2021-11-17 13:25:06 +0300

    Algorithms. Course 1. Week 4. 02-binary-search-with-d FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/02-binary-search-with-d/Project/02-binary-search-with-d.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/02-binary-search-with-d/Project/02-binary-search-with-d.cpp
@@ -1,36 +1,69 @@
-#include <iostream>
-#include <cassert>
-#include <vector>
+#include "test_runner.h"
+#include "profile.h"
+#include "random_generate.h"
 
-using std::vector;
+template <typename RandomIt, typename Value>
+RandomIt my_binary_search(RandomIt first, RandomIt last, Value element) {
+    RandomIt save_end = last;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+    while (first != last) {
+        RandomIt middle = first + ((last - first) / 2);
+        //cerr << *middle << endl;
+        if (element < *middle) {
+            last = middle;
+        } else if (element > *middle) {
+            first = ++middle;
+        } else {
+            if (*prev(middle) == element) {
+                return my_binary_search(first, --middle, element);
+            } else {
+                return middle;
+            }
+        }
+    }
+
+    return save_end;
 }
 
-int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
-  }
-  return -1;
+
+vector<int> bunch_of_search(const vector<int> data, const vector<int> &to_find) {
+    vector<int> temporary;
+    for (const auto &element : to_find) {
+        if (my_binary_search(data.begin(), data.end(), element) == data.end()) {
+            temporary.push_back(-1);
+        } else {
+            temporary.push_back(distance(data.begin(),
+                                         my_binary_search(data.begin(), data.end(), element))
+                                );
+        }
+    }
+    return temporary;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+    {   vector<int> data = { //           9
+            1, 5, 5, 5, 5, 8, 12, 13, 13, 13, 13, 13,
+        },
+        to_find = {
+            8, 1, 23, 1, 10, 13
+        },
+        expected = {
+            5, 0, -1, 0, -1, 7
+        };
+        LOG_DURATION("First banch of tests");
+        ASSERT_EQUAL(bunch_of_search(data, to_find), expected);
+    }
+    {
+        vector<int> data = {
+            1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 6
+        },
+        to_find = {
+            1, 2, 3, 4, 5, 6
+        },
+        expected = {
+            0, 1, 2, 3, 4, 11
+        };
+        LOG_DURATION("Second banch of tests");
+        ASSERT_EQUAL(bunch_of_search(data, to_find), expected);
+    }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/02-binary-search-with-d/Solution/02-binary-search-with-d.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/02-binary-search-with-d/Solution/02-binary-search-with-d.cpp
@@ -4,33 +4,59 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+template <typename RandomIt, typename Value>
+RandomIt my_binary_search(RandomIt first, RandomIt last, Value element) {
+    RandomIt save_end = last;
+
+    while (first != last) {
+        RandomIt middle = first + ((last - first) / 2);
+        //cerr << *middle << endl;
+        if (element < *middle) {
+            last = middle;
+        } else if (element > *middle) {
+            first = ++middle;
+        } else {
+            if (*prev(middle) == element) {
+                return my_binary_search(first, --middle, element);
+            } else {
+                return middle;
+            }
+        }
+    }
+
+    return save_end;
 }
 
-int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
-  }
-  return -1;
+
+vector<int> bunch_of_search(const vector<int> data, const vector<int> &to_find) {
+    vector<int> temporary;
+    for (const auto &element : to_find) {
+        if (my_binary_search(data.begin(), data.end(), element) == data.end()) {
+            temporary.push_back(-1);
+        } else {
+            temporary.push_back(distance(data.begin(),
+                                         my_binary_search(data.begin(), data.end(), element))
+                                );
+        }
+    }
+    return temporary;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+    int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cin >> a[i];
+    }
+    int m;
+    std::cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        std::cin >> b[i];
+    }
+
+    for (const auto &item : bunch_of_search(a, b)) {
+        std::cout << item << " ";
+    }
 }
