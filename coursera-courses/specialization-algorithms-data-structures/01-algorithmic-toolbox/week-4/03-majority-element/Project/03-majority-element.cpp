3a938fdc76bd70f8710ca5f641cba04bb4300e0e
Xelerezex <nikitavalderman@gmail.com>
2021-11-23 14:43:52 +0300

    Algorithms. Course 1. Week 4. 03-majority-element FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/03-majority-element/Project/03-majority-element.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/03-majority-element/Project/03-majority-element.cpp
@@ -1,22 +1,59 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include "test_runner.h"
+#include "profile.h"
+#include "random_generate.h"
 
-using std::vector;
+int get_majority_element(vector<int> &input) {
+    map<int, int> hash;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+    for (auto &item : input) {
+        if (auto it = hash.find(item); it == hash.end()) {
+            hash[item] = 1;
+        } else {
+            hash[item] += 1;
+        }
+    }
+
+    int maxn = max_element(hash.begin(), hash.end(),
+        [] (pair<int, int> lhs, pair<int, int> rhs) {
+            return lhs.second < rhs.second;
+        }
+    )->second;
+
+    // cerr << hash << endl;
+    return ((input.size() / 2) < maxn) ? 1 : 0 ;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    {
+        vector<int> input = {
+            2, 3, 9, 2, 2,
+        };
+
+        LOG_DURATION("Test #1");
+        ASSERT_EQUAL(get_majority_element(input), 1);
+    }
+    {
+        vector<int> input = {
+            1, 2, 3, 4,
+        };
+
+        LOG_DURATION("Test #2");
+        ASSERT_EQUAL(get_majority_element(input), 0);
+    }
+    {
+
+        vector<int> temp_rand = generate_random_vector(49'999, 0, 1'000'000'000, 420);
+        vector <int> temp(50'001, 1'000'000'000);
+        vector<int> input;
+
+        for (size_t it = 0; it < 50'002; ++it) {
+            input.push_back(move(*(--temp_rand.end())));
+            input.push_back(move(*(--temp.end())));
+            temp_rand.pop_back();
+            temp.pop_back();
+        }
+
+        LOG_DURATION("Test #3");
+        ASSERT_EQUAL(get_majority_element(input), 0);
+    }
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/03-majority-element/Solution/03-majority-element.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/03-majority-element/Solution/03-majority-element.cpp
@@ -1,22 +1,44 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <map>
 
+using std::max_element;
 using std::vector;
+using std::pair;
+using std::map;
+using std::cin;
+using std::cout;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+int get_majority_element(vector<int> &input) {
+    map<int, int> hash;
+
+    for (auto &item : input) {
+        if (auto it = hash.find(item); it == hash.end()) {
+            hash[item] = 1;
+        } else {
+            hash[item] += 1;
+        }
+    }
+
+    int maxn = max_element(hash.begin(), hash.end(),
+        [] (pair<int, int> lhs, pair<int, int> rhs) {
+            return lhs.second < rhs.second;
+        }
+    )->second;
+
+    // cerr << hash << endl;
+    return ((input.size() / 2) < maxn) ? 1 : 0 ;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        cin >> a[i];
+    }
+
+    cout << get_majority_element(a) << '\n';
 }
