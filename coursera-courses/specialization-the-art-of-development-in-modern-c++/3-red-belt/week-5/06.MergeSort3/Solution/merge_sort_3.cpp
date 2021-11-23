f9fb4b26b4945c0732b0be85ea9a2b65ac089071
Xelerezex <nikitavalderman@gmail.com>
2021-11-23 15:37:19 +0300

    C++. Red Belt. Week 5. 06.MergeSort3 FULLY DONE!

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-5/06.MergeSort3/Solution/merge_sort_3.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-5/06.MergeSort3/Solution/merge_sort_3.cpp
@@ -7,8 +7,38 @@ using namespace std;
 
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end) {
-    // Напишите реализацию функции,
-    // не копируя сортируемые элементы
+    if (distance(range_begin, range_end) < 2) {
+        return;
+    } else {
+        vector<typename RandomIt::value_type> elements (
+            make_move_iterator(range_begin),
+            make_move_iterator(range_end)
+        );
+
+        auto itr_part_1 = elements.begin() + (elements.size() / 3);
+        auto itr_part_2 = itr_part_1 + (elements.size() / 3);
+
+        MergeSort(elements.begin(), itr_part_1);
+        MergeSort(itr_part_1, itr_part_2);
+        MergeSort(itr_part_2, elements.end());
+
+        vector<typename RandomIt::value_type> temp;
+
+        merge(
+            make_move_iterator(elements.begin()),
+            make_move_iterator(itr_part_1),
+            make_move_iterator(itr_part_1),
+            make_move_iterator(itr_part_2),
+            back_inserter(temp)
+
+        );
+        merge(
+            make_move_iterator(temp.begin()),
+            make_move_iterator(temp.end()),
+            make_move_iterator(itr_part_2),
+            make_move_iterator(elements.end()),
+            range_begin);
+    }
 }
 
 void TestIntVector() {
