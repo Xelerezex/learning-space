6e897df2d7ca7cb2214c2a9705dde3553aeb7a17
Xelerezex <nikitavalderman@gmail.com>
2021-11-29 15:56:18 +0300

    C++. Red Belt. Week 5. 08.MatrixSum. Adding project struture.

---
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/3-red-belt/week-5/08.MatrixSum/Solution/matrix_sum.cpp
@@ -0,0 +1,24 @@
+#include "test_runner.h"
+#include <vector>
+using namespace std;
+
+int64_t CalculateMatrixSum(const vector<vector<int>>& matrix) {
+    int64_t result = 0;
+
+    return result;
+}
+
+void TestCalculateMatrixSum() {
+    const vector<vector<int>> matrix = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+    ASSERT_EQUAL(CalculateMatrixSum(matrix), 136);
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestCalculateMatrixSum);
+}
