3125fbd67587a0d0b150c067e490a715889105d6
Xelerezex <nikitavalderman@gmail.com>
2022-01-28 18:23:21 +0300

    C++. Brown Belt. Week 1. 01.PriorityCollection. Adding project structure.

---
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-1/01.PriorityCollection/Solution/headers.h
@@ -0,0 +1,52 @@
+#pragma once
+
+//-----------------DATA-TYPES
+#include <string_view>
+#include <utility>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <array>
+#include <list>
+#include <set>
+#include <map>
+
+//-----------------MATH
+#include <cmath>
+
+//-----------------ALGORITHMIC
+#include <algorithm>
+#include <numeric>
+
+//----------------INPUT-OUTPUT
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+//----------------EXCEPTIONS
+#include <exception>
+#include <stdexcept>
+
+//----------------MEMORY/TIME
+#include <memory>
+#include <chrono>
+
+//----------------ASYNC
+#include <shared_mutex>
+#include <functional>
+#include <thread>
+#include <future>
+#include <mutex>
+
+//----------------RANDOM
+#include <random>
+
+//----------------ITERATION
+#include <iterator>
+
+//----------------NAMESPACES
+using namespace std::chrono;
+using namespace std;
+
