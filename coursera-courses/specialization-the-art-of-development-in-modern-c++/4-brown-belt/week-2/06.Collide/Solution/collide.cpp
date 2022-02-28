f6e6abf7f02e605187949c80468491a3a86a4cfc
Xelerezex <nikitavalderman@gmail.com>
2022-02-28 16:55:00 +0300

    C++. Brown Belt. Week 2. 06.Collide. Adding project structure.

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-2/06.Collide/Solution/collide.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-2/06.Collide/Solution/collide.cpp
@@ -11,6 +11,7 @@ using namespace std;
 // Определите классы Unit, Building, Tower и Fence так, чтобы они наследовались от
 // GameObject и реализовывали его интерфейс.
 
+
 class Unit
 {
     public:
