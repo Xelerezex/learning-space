b3a5c58eec62f54cc5120af7f07a6f8ebb89c0a9
Xelerezex <nikitavalderman@gmail.com>
2022-03-01 18:18:12 +0300

    Algorithms. Course 1. Week 4. 06-organizing-lottery FULLY DONE!

--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/06-organizing-lottery/Project/06-organizing-lottery.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/06-organizing-lottery/Project/06-organizing-lottery.cpp
@@ -1,38 +1,115 @@
-#include <iostream>
-#include <vector>
+#include "test_runner.h"
 
-using std::vector;
+std::vector<int> fast_count_segments(std::vector<std::pair<int, int>> &ranges, std::vector<int> &points)
+{
+    std::vector<int> result(points.size());
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  //write your code here
-  return cnt;
-}
+    std::vector<std::pair<int, int>> vec;
+    std::unordered_map<int, int> point_to_counts;
 
-vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
-      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+    vec.reserve(ranges.size() * 2 + points.size());
+    point_to_counts.reserve(points.size());
+
+    for (const auto &pair : ranges)
+    {
+        vec.push_back({pair.first, 0});
+        vec.push_back({pair.second, 2});
     }
-  }
-  return cnt;
+
+    for (const auto &item : points)
+    {
+        vec.push_back({item, 1});
+    }
+
+    std::sort(vec.begin(), vec.end(),
+        [](std::pair<int, int>& lhs, std::pair<int, int>& rhs) {
+            if (lhs.first == rhs.first)
+            {
+                return lhs.second < rhs.second;
+            }
+            else
+            {
+                return lhs.first < rhs.first;
+            }
+    });
+
+    int counter = 0;
+
+    for (const auto &pair : vec)
+    {
+        if (pair.second == 0)
+        {
+            counter++;
+        }
+        else if (pair.second == 1)
+        {
+            point_to_counts.insert({pair.first, counter});
+        }
+        else /*if (pair.second == 2)*/
+        {
+            counter--;
+        }
+    }
+
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        result[i] = point_to_counts[points[i]];
+    }
+
+    return result;
 }
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
-    std::cin >> starts[i] >> ends[i];
-  }
-  vector<int> points(m);
-  for (size_t i = 0; i < points.size(); i++) {
-    std::cin >> points[i];
-  }
-  //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
-  for (size_t i = 0; i < cnt.size(); i++) {
-    std::cout << cnt[i] << ' ';
-  }
+
+int main()
+{
+    {
+        std::vector<std::pair<int, int>> input_segments {
+            {0, 5},
+            {7, 10}
+        };
+
+        std::vector<int> input_points {
+            1, 6, 11
+        };
+
+        std::vector<int> expected {
+            1, 0, 0
+        };
+
+        ASSERT_EQUAL(fast_count_segments(input_segments, input_points), expected);
+    }
+    {
+        std::vector<std::pair<int, int>> input_segments {
+            {-10, 10},
+        };
+
+        std::vector<int> input_points {
+            -100, 100, 0
+        };
+
+        std::vector<int> expected {
+            0, 0, 1
+        };
+
+        ASSERT_EQUAL(fast_count_segments(input_segments, input_points), expected);
+    }
+    {
+        std::vector<std::pair<int, int>> input_segments {
+            {0, 5},
+            {-3, 2},
+            {7, 10},
+        };
+
+        std::vector<int> input_points {
+            1, 6
+        };
+
+        std::vector<int> expected {
+            2, 0
+        };
+
+        ASSERT_EQUAL(fast_count_segments(input_segments, input_points), expected);
+    }
+
+    std::cerr << "OK!" << std::endl;
 }
--- coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/06-organizing-lottery/Solution/06-organizing-lottery.cpp
+++ coursera-courses/specialization-algorithms-data-structures/01-algorithmic-toolbox/week-4/06-organizing-lottery/Solution/06-organizing-lottery.cpp
@@ -1,38 +1,96 @@
+#include <unordered_map>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
-using std::vector;
+std::vector<int> fast_count_segments(std::vector<std::pair<int, int>> &ranges, std::vector<int> &points)
+{
+    std::vector<int> result(points.size());
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  //write your code here
-  return cnt;
-}
+    std::vector<std::pair<int, int>> vec;
+    std::unordered_map<int, int> point_to_counts;
 
-vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
-      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+    vec.reserve(ranges.size() * 2 + points.size());
+    point_to_counts.reserve(points.size());
+
+    for (const auto &pair : ranges)
+    {
+        vec.push_back({pair.first, 0});
+        vec.push_back({pair.second, 2});
     }
-  }
-  return cnt;
-}
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
-    std::cin >> starts[i] >> ends[i];
-  }
-  vector<int> points(m);
-  for (size_t i = 0; i < points.size(); i++) {
-    std::cin >> points[i];
-  }
-  //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
-  for (size_t i = 0; i < cnt.size(); i++) {
-    std::cout << cnt[i] << ' ';
-  }
+    for (const auto &item : points)
+    {
+        vec.push_back({item, 1});
+    }
+
+    std::sort(vec.begin(), vec.end(),
+        [](std::pair<int, int>& lhs, std::pair<int, int>& rhs) {
+            if (lhs.first == rhs.first)
+            {
+                return lhs.second < rhs.second;
+            }
+            else
+            {
+                return lhs.first < rhs.first;
+            }
+    });
+
+    int counter = 0;
+
+    for (const auto &pair : vec)
+    {
+        if (pair.second == 0)
+        {
+            counter++;
+        }
+        else if (pair.second == 1)
+        {
+            point_to_counts.insert({pair.first, counter});
+        }
+        else /*if (pair.second == 2)*/
+        {
+            counter--;
+        }
+    }
+
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        result[i] = point_to_counts[points[i]];
+    }
+
+    return result;
+}
+int main()
+{
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int ranges_size, points_size;
+    std::cin >> ranges_size >> points_size;
+    std::vector<std::pair<int, int>> ranges;
+    ranges.reserve(ranges_size);
+
+    int start, end;
+    for (size_t it = 0; it < ranges_size; ++it)
+    {
+        std::cin >> start >> end;
+        if (start <= end)
+        {
+            ranges.push_back({start, end});
+        }
+    }
+
+    std::vector<int> points(points_size);
+    for (size_t it = 0; it < points_size; ++it)
+    {
+        std::cin >> points[it];
+    }
+
+    std::vector<int> result = fast_count_segments(ranges, points);
+
+    for (size_t it = 0; it < result.size(); ++it)
+    {
+        std::cout << result[it] << " ";
+    }
 }
