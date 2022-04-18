2db6b3688b7329bd48da9971e0357a2a2de0b268
Xelerezex <nikitavalderman@gmail.com>
2022-04-18 20:00:11 +0300

    C++. Brown Belt. Week 4. 04.Booking FULLY DONE!

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-4/04.Booking/Solution/booking.h
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-4/04.Booking/Solution/booking.h
@@ -1,7 +1,50 @@
 #pragma once
 
-template<typename Provider>
-class Booking<Provider>
+#include <utility>
+
+namespace RAII
 {
 
+
+template<typename Provider>
+class Booking
+{
+    private:
+        using BookingId = typename Provider::BookingId;
+
+        Provider* provider;
+        BookingId id;
+    public:
+        Booking(Provider* p, BookingId& c)
+            : provider(p)
+            , id(c)
+        {
+        }
+
+        Booking(const Booking&) = delete;
+        Booking(Booking&& other)
+            : provider(other.provider)
+            , id(other.id)
+        {
+            other.provider = nullptr;
+        }
+        Booking& operator = (const Booking&) = delete;
+
+        Booking& operator = (Booking&& other)
+        {
+            std::swap(provider, other.provider);
+            std::swap(id,       other.id);
+            return *this;
+        }
+
+
+        ~Booking()
+        {
+            if (provider != nullptr)
+            {
+                provider->CancelOrComplete(*this);
+            }
+        }
 };
+
+}
