cf9a8318bbaefabd71a75368b0e514ea51c31823
Xelerezex <nikitavalderman@gmail.com>
2022-04-19 17:15:51 +0300

    C++. Brown Belt. Week 5. 01.PrintStats FULLY DONE!

--- coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-5/01.PrintStats/Solution/main.cpp
+++ coursera-courses/specialization-the-art-of-development-in-modern-c++/4-brown-belt/week-5/01.PrintStats/Solution/main.cpp
@@ -36,7 +36,49 @@ int ComputeMedianAge(InputIt range_begin, InputIt range_end)
 
 void PrintStats(vector<Person> persons)
 {
+    std::cout << "Median age = " << ComputeMedianAge(persons.begin(), persons.end()) << std::endl;;
 
+    std::cout << "Median age for females = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [] (const Person& person) {
+            return person.gender == Gender::FEMALE;
+        }))
+    << std::endl;
+
+    std::cout << "Median age for males = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [] (const Person& person) {
+            return person.gender == Gender::MALE;
+        }))
+    << std::endl;
+
+    std::cout << "Median age for employed females = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [](const Person& person) {
+            return person.gender == Gender::FEMALE && person.is_employed == true;
+        }))
+    << std::endl;
+
+    std::cout << "Median age for unemployed females = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [](const Person& person) {
+            return person.gender == Gender::FEMALE && person.is_employed == false;
+        }))
+    << std::endl;
+
+    std::cout << "Median age for employed males = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [](const Person& person) {
+            return person.gender == Gender::MALE && person.is_employed == true;
+        }))
+    << std::endl;
+
+    std::cout << "Median age for unemployed males = "
+    << ComputeMedianAge(persons.begin(), std::partition(persons.begin(), persons.end(),
+        [](const Person& person) {
+            return person.gender == Gender::MALE && person.is_employed == false;
+        }))
+    << std::endl;
 }
 
 int main()
