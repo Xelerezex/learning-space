daf4650a7d574f35b031d4026ea7e2d4a4224206
Xelerezex <nikitavalderman@gmail.com>
2023-03-08 19:54:08 +0300

    C++. Multithreading. Containers. Create minimal example.

--- stepik-courses/stepik-c++-multithread/02-containers-smart-ptr/main.cpp
+++ stepik-courses/stepik-c++-multithread/02-containers-smart-ptr/main.cpp
@@ -1,3 +1,24 @@
-int main()
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(TestGroupName, Subtest_1) 
 {
+  ASSERT_TRUE(1 == 1);
+}
+
+TEST(TestGroupName, Subtest_2)
+{
+  ASSERT_FALSE('b' == 'b');
+  cout << "continue test after failure" << endl;
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
